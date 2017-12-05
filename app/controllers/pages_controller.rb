5922953498113e8496ca920385367c22ec930e45
danielfsokol <danielfsokol@gmail.com>
2017-12-05 11:43:31 -0500

    search all

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,14 @@ class PagesController < ApplicationController
   def search
     @manufacturers = Manufacturer.all.order(:name)
     @shops = []
+
     if params[:commit] && params[:commit]=="Search" && params[:manufacturer].present?
 
       @shops = Shop.all
 
       if params[:match] == "All"
         @shops = @shops.select do |s|
-          (s.manufacturers.map(&:name) & params[:manufacturer]) == params[:manufacturer]
+          (s.manufacturers.map(&:name) & params[:manufacturer]).sort == params[:manufacturer].sort
         end
       else
         @shops = @shops.select do |s|
